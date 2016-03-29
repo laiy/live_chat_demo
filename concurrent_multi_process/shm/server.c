135a264124fc9e40ea300ca50036e2087e6c01d4
laiy <ly.franky@gmail.com>
2016-03-29 16:44:39 +0800

    add mutex control to global thread variable client

--- concurrent_multi_process/shm/server.c
+++ concurrent_multi_process/shm/server.c
@@ -10,18 +10,19 @@
 #include <time.h>
 #include <pthread.h>
 #include "awesome_handler.h"
+#include <sys/ipc.h>
+#include <sys/shm.h>
 
 #define LISTENQ 5
 #define MAX_CONNECTIONS 1024
 
-int client[MAX_CONNECTIONS];
-static void *boardcast(void *);
-pthread_mutex_t client_mutex = PTHREAD_MUTEX_INITIALIZER;
+int *client;
+
+void boardcast(void *, int shmid);
 
 int main() {
-    int listenfd, i;
+    int listenfd, i, shmid, pid;
     long connfd;
-    pthread_t tid;
     socklen_t clilen;
     struct sockaddr_in client_address, server_address;
     listenfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -31,40 +32,56 @@ int main() {
     server_address.sin_port = htons(SERVER_PORT);
     bind(listenfd, (struct sockaddr *)&server_address, sizeof(server_address));
     listen(listenfd, LISTENQ);
+    if ((shmid = shmget(IPC_PRIVATE, MAX_CONNECTIONS * sizeof(int), IPC_CREAT | IPC_EXCL)) < 0)
+        error("shm error");
+    client = (int*)shmat(shmid, 0, 0);
     for (i = 0; i < MAX_CONNECTIONS; i++)
         client[i] = -1;
     for (;;) {
         clilen = sizeof(client_address);
         connfd = accept(listenfd, (struct sockaddr *)&client_address, &clilen);
-        pthread_mutex_lock(&client_mutex);
+        if (shmctl(shmid, SHM_LOCK, 0) < 0)
+            error("shmctl error");
         for (i = 0; i < MAX_CONNECTIONS; i++)
             if (client[i] < 0) {
                 client[i] = connfd;
                 break;
             }
-        pthread_mutex_unlock(&client_mutex);
+        if (shmctl(shmid, SHM_UNLOCK, 0) < 0)
+            error("shmctl error");
         if (i == MAX_CONNECTIONS)
             error("too many clients");
-        pthread_create(&tid, NULL, &boardcast, (void *)connfd);
+        if ((pid = fork()) == 0) {
+            close(listenfd);
+            boardcast((void *)connfd, shmid);
+            exit(0);
+        } else
+            continue;
     }
+    if (shmctl(shmid, IPC_RMID, NULL) < 0)
+        error("shmdt error");
     return 0;
 }
 
-static void *boardcast(void *arg) {
+void boardcast(void *arg, int shmid) {
     ssize_t n;
     char buf[MAXLINE];
     int j, tmpfd;
     pthread_detach(pthread_self());
     while ((n = read((long)arg, buf, MAXLINE)) > 0) {
-        pthread_mutex_lock(&client_mutex);
+        if (shmctl(shmid, SHM_LOCK, 0) < 0)
+            error("shmctl error");
         for (j = 0; j < MAX_CONNECTIONS; j++) {
             if ((tmpfd = client[j]) < 0 || (long)arg == tmpfd)
                 continue;
+            printf("%d b\n", tmpfd);
             write(tmpfd, buf, n);
         }
-        pthread_mutex_unlock(&client_mutex);
+        if (shmctl(shmid, SHM_UNLOCK, 0) < 0)
+            error("shmctl error");
     }
+    if (shmdt(client) < 0)
+        error("shmdt error");
     close((long)arg);
-    return NULL;
 }
 
